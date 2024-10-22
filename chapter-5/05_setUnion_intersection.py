18b8eded275a0d46a19d084cc109b2e0f96ff3fa
vaibhav <vaibhavginnalwar@gmail.com>
2024-10-22 13:01:24 +0530

    learn dictionary and sets

---
+++ chapter-5/05_setUnion_intersection.py
@@ -0,0 +1,5 @@
+s1 = {1, 45, 6, 78}  
+s2 = {7, 8, 1, 78}
+
+print(s1.union(s2))  #take unic value from 2
+print(s1.intersection(s2))  #take common value from 2
---
+++ chapter-5ps/07problem7.py
@@ -0,0 +1,3 @@
+s = {8, 7, 12, "Harry", [1,2]}
+
+s[4][0] = 9 #not able to change value of set and not able to add list in sets also
